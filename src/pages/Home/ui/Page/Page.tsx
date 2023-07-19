d1ebd2ff9c17c74e8fdb8e2fc59144ae77ab5b68
Aleksey Popov <oluqom@proton.me>
2023-07-19 13:05:06 +0700

    feat: update Home page

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -1,28 +1,40 @@
 import { FC } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel } from "swiper/modules";
+
+import "swiper/css";
 
 const Home: FC = () => {
   return (
     <>
       <section>
-        <div className="hero min-h-[calc(100vh-64px)] bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="/images/hero.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">Welcome</h1>
-              <p className="py-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-                minima laboriosam maxime sed dignissimos harum provident itaque
-                fugiat. A repellat aliquid inventore dolor tempora, omnis
-                perferendis aspernatur quo nisi excepturi. Ex, ullam odio iusto
-                esse necessitatibus doloremque repudiandae!
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div>
-        </div>
+        <Swiper
+          direction={"vertical"}
+          slidesPerView={1}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Mousewheel]}
+          className="h-full"
+        >
+          <SwiperSlide>
+            <h1 className="uppercase xl:w-[1172px] z-30 text-8xl text-white font-black leading-none">
+              Embrace scarcity, unlock self-control
+            </h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1 className="uppercase xl:w-[1065px] text-8xl text-white font-black leading-none">
+              Rediscover the power of restraint
+            </h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1 className="uppercase xl:w-[834px] text-8xl text-white font-black leading-none">
+              Your guide to a balanced and intentional lifestyle
+            </h1>
+          </SwiperSlide>
+        </Swiper>
       </section>
     </>
   );
