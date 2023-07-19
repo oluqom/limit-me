9fc49da543e93c5afc2150a2d58c97e4c3995425
Aleksey Popov <oluqom@proton.me>
2023-07-19 14:09:47 +0700

    feat: update Login

--- src/pages/Auth/ui/Login/Login.tsx
+++ src/pages/Auth/ui/Login/Login.tsx
@@ -5,13 +5,18 @@ const Auth = () => {
 const Login = () => {
   return (
     <>
-      <div>
-        <img />
-        <div>
-          <button onClick={Auth}>login</button>
-          <div>
-            <input type="text" placeholder="login" />
-            <input type="password" placeholder="password" />
+      <div className="flex items-center gap-x-24">
+        <img src="/images/login.webp" />
+        <div className="flex w-[350px] flex-col items-start">
+          <button
+            className="mb-5 text-white font-black text-4xl leading-none"
+            onClick={Auth}
+          >
+            login
+          </button>
+          <div className="flex flex-col w-[350px] gap-y-3">
+            <input className="input" type="text" placeholder="login" />
+            <input className="input" type="password" placeholder="password" />
           </div>
         </div>
       </div>
