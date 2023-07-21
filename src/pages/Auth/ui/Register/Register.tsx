71fe961effc11d9dca85e24380b59b5794d26e9b
Aleksey Popov <oluqom@proton.me>
2023-07-21 19:05:38 +0700

    feat: update Register

--- src/pages/Auth/ui/Register/Register.tsx
+++ src/pages/Auth/ui/Register/Register.tsx
@@ -5,14 +5,19 @@ const Auth = () => {
 const Register = () => {
   return (
     <>
-      <div>
-        <img />
+      <div className="flex items-center gap-x-24">
+        <img src="/images/register.webp" />
         <div>
-          <button onClick={Auth}>register</button>
-          <div>
-            <input type="text" placeholder="name" />
-            <input type="text" placeholder="login" />
-            <input type="password" placeholder="password" />
+          <button
+            className="mb-5 text-4xl font-black text-white"
+            onClick={Auth}
+          >
+            register
+          </button>
+          <div className="flex flex-col gap-y-3">
+            <input className="input" type="text" placeholder="name" />
+            <input className="input" type="text" placeholder="login" />
+            <input className="input" type="password" placeholder="password" />
           </div>
         </div>
       </div>
