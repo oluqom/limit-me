67446cb4fbe46f39997e2f996fce340bca5b9783
Aleksey Popov <oluqom@proton.me>
2023-07-29 19:38:57 +0700

    feat: update Login

--- src/features/Auth/ui/Login/Login.tsx
+++ src/features/Auth/ui/Login/Login.tsx
@@ -1,20 +1,31 @@
-const Auth = () => {
-  console.log("Login");
-};
+import { useAppSelector, useAppDispatch } from "@/app/hooks";
+import { setUser } from "../../AuthSlice";
 
 const Login = () => {
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  const user = useAppSelector((state) => state.auth.user);
+  const loading = useAppSelector((state) => state.auth.loading);
+  const error = useAppSelector((state) => state.auth.error);
+  const dispatch = useAppDispatch();
+
+  const Auth = () => {
+    dispatch(setUser([{ name: "admin", login: "admin", password: "admin" }]));
+    console.log("Auth" + isAuthenticated + "n/ user:" + user);
+    console.log("Loading" + loading);
+  };
+
   return (
     <>
       <div className="flex items-center gap-x-24">
         <img src="/images/login.webp" />
         <div className="flex w-[350px] flex-col items-start">
           <button
-            className="mb-5 text-white font-black text-4xl leading-none"
+            className="mb-5 text-4xl font-black leading-none text-white"
             onClick={Auth}
           >
             login
           </button>
-          <div className="flex flex-col w-[350px] gap-y-3">
+          <div className="flex w-[350px] flex-col gap-y-3">
             <input className="input" type="text" placeholder="login" />
             <input className="input" type="password" placeholder="password" />
           </div>
